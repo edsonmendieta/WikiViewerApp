832e429f530139192a93c5eb6df614076e58715e
edsonmendieta <emendietaj@gmail.com>
2016-09-24 14:40:11 -0700

    added functioning keyframe animation for search-bar expansion on click.

--- scripts.js
+++ scripts.js
@@ -6,12 +6,25 @@ function openClose() {
 
     var searchBar = document.getElementById('searchBar');
 
-    // var handle = document.getElementById('line');
-
     if(searchBar.getAttribute('class') == 'closed') {
 
-        
+        // links 'expand' keyframe to element
+        searchBar.style.animationName = "expand";
 
-        // handle.setAttribute('id', 'lineGone');
+        // Keeps circle from expanding early due to added padding.
+        setTimeout(textPadding, 300);
+
+        function textPadding() {
+
+            // adds space b/w border and start of text
+            searchBar.style.paddingLeft = "15px";
+
+            // reverts cursor style back to text version
+            searchBar.style.cursor = "text";
+        }
+
+        // denotes that search-bar is open
+        searchBar.setAttribute('class', 'open');
+
     }
 }
